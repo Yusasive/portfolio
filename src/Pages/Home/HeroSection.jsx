500aa736f434fc922a7e56f278dc751b60b41187
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-09-22 23:36:12 +0100

    updated

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,18 +3,17 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Abdullahi Yusuf</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <span className="hero--section-title--color">FrontEnd</span>{" "}
             <br />
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          I am a committed FrontEnd Developer with 2years of experience in the tech industry. I am passionate and responsible for the design and implementation of user interfaces (UIs) and UI components using React, a front-end JavaScript library.
           </p>
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
+       <a href="https://wa.link/dn512o"> <button className="btn btn-primary">Get In Touch</button></a>
       </div>
       <div className="hero--section--img">
         <img src="./img/hero_img.png" alt="Hero Section" />
