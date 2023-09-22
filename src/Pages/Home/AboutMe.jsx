6295a93a831b67133f48a76563e0c6131c14f02f
Yuusuf Abdullahi <yusufabdullahi2910@gmail.com>
2023-09-22 23:55:37 +0100

    updated

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,10 +9,11 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          I am a committed FrontEnd Developer with 2years of
+           experience in the tech industry. I am passionate and 
+           responsible for the design and implementation of user 
+           interfaces (UIs) and UI components using React, a 
+           front-end JavaScript library.
           </p>
           <p className="hero--section-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
